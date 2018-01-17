5721cf9b669b3213c180a962723250e16c48093b
Emre-Kul <emrekul35@gmail.com>
2018-01-18 00:05:34 +0300

    readme and graph url changes

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import os
-import itertools
-import requests
-import json
-import time
+import os,requests,json,time
 from bs4 import BeautifulSoup
 
-COIN_GRAPH_COUNT = 100
-SAVE_FOLDER = "../coin-data/"
-SLEEP_TIME_BETWEEN_REQUESTS = 2
+COIN_GRAPH_COUNT = 10
+SAVE_FOLDER = "coin-data/"
+SLEEP_TIME_BETWEEN_REQUESTS = 1
+PROXIE_ON = False
+PROXIES = {
+	'https' : 'x:x'	
+}
 
 class cspider:#static class
 	def create_folder(folder_name):
@@ -29,17 +29,20 @@ class cspider:#static class
 	def get_page(url):
 		print("Getting : "+url)
 		try:
-			req = requests.get(url)
+			if PROXIE_ON :
+				req = requests.get(url,proxies=PROXIES)
+			else : 
+				req = requests.get(url)
 			req.raise_for_status()
 		except requests.exceptions.HTTPError as e:
-			if(req.headers['Retry-After']):#sometimes this returns Date i should control it too
+			if 'Retry-After' in req.headers :#sometimes this returns Date i should control it too
 				exit("Banned, Try Again After " + str( int(int(req.headers['Retry-After'])/60) ) + " Minutes")
-			else :
-				exit(e)
+			#print(req.headers)
+			exit(e)
 		return req
 	
 	def get_coin_graph(coin_url):
-		URL = "https://graphs.coinmarketcap.com"+coin_url
+		URL = "https://graphs2.coinmarketcap.com"+coin_url
 		page = cspider.get_page(URL)
 		json_data = json.loads(page.text)
 		return json_data['price_usd']
@@ -80,11 +83,21 @@ class cspider:#static class
 
 	def run():
 		coins = cspider.get_all_coins()[0:COIN_GRAPH_COUNT]
-		json_coins = json.dumps(coins,indent=4)
+		'''
 		for coin in coins:
-			coin['graph'] = cspider.get_coin_graph(coin['url'])
+			coin.update( {'graph' : cspider.get_coin_graph(coin['url']) })
 			time.sleep(SLEEP_TIME_BETWEEN_REQUESTS)
+		json_coins = json.dumps(coins)
+		'''
+		json_coins = json.dumps(coins,indent=2)
 		cspider.save_data_to_file(json_coins,SAVE_FOLDER,"coins")
+		count = 1
+		for coin in coins:
+			print(count,end=" ")
+			coin_graph = cspider.get_coin_graph(coin['url'])
+			cspider.save_data_to_file(json.dumps(coin_graph),SAVE_FOLDER,coin['symbol'])
+			count += 1
+			time.sleep(SLEEP_TIME_BETWEEN_REQUESTS)
 		print("All Coins Saved")
 
 cspider.run();
